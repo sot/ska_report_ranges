2a8e7118f220df0aba34b4972cdec1da941b86e0
Jean Connelly <jconnelly@cfa.harvard.edu>
2020-10-14 12:59:22 -0400

    Fix whitespace in setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 from setuptools import setup
 setup(name='Ska.report_ranges',
-      author = 'Jean Connelly',
+      author='Jean Connelly',
       description='Time Range manipulation for report generation',
-      author_email = 'jconnelly@cfa.harvard.edu',
-      url = 'http://cxc.harvard.edu/mta/ASPECT/tool_doc/pydocs/Ska.report_ranges.html',
+      author_email='jconnelly@cfa.harvard.edu',
+      url='http://cxc.harvard.edu/mta/ASPECT/tool_doc/pydocs/Ska.report_ranges.html',
       use_scm_version=True,
       zip_safe=False,
       packages=['Ska', 'Ska.report_ranges'],
